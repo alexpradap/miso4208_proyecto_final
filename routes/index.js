e13bd4d58b4f1e4e31addb8a25197f0b77ccafe4
Eday Gonzalez <edaygonzalez@ingenieros.com>
2020-10-18 22:29:38 -0500

    Se ajusta el proy para publicar mensajes y no ejecutar test. La responsabilidad queda en el Worker

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,21 @@ var router = express.Router();
 var connection  = require('../lib/db');
 var mqtt = require('mqtt')
 
+var client = mqtt.connect('mqtt://localhost');
+client.on('connect', function () {
+    console.log('Connected via MQTT')
+    client.subscribe(['starter','terminator']);
+})
+
+router.get('/notifications',(req,res,next)=>{
+    console.log("Iniciando Notifications")
+    client.on('message', function (topic, message) {
+        // message is Buffer
+       console.log("Tratando de obtener mensajes")
+       //client.end()
+       res.render('notifications',{ data: 1})
+    }) 
+})
 router.get('/index', (req, res, next) => {
     connection.query('SELECT * FROM test_types ',function(err,rows) {
         if(err){
@@ -19,11 +34,7 @@ router.get('/index', (req, res, next) => {
     
 });
 
-var client = mqtt.connect('mqtt://localhost');
-client.on('connect', function () {
-    console.log('Connected via MQTT')
-    client.subscribe(['starter','terminator']);
-})
+
 router.get('/start', (req, res) => {
 
     var arrayTest = {
@@ -37,41 +48,16 @@ router.get('/start', (req, res) => {
     let testId = req.query.test;
     let currentTest = arrayTest[testId];
 
-
-    child_process.exec(`ECHO "Execution of ${currentTest} in progress..." > result.log`).toString('utf8');
-    child_process.exec(`rm cypress/videos/${currentTest}.spec.js.mp4`).toString('utf8');
-
-    //child_process.exec(`npx cypress run --quiet --headless --spec "cypress/integration/${currentTest}.spec.js" >> result_${currentTest}.log`).toString('utf8');
-    const ls=child_process.spawn('npx', ['cypress','run','--quiet', '--headless', '--spec',`cypress/integration/${currentTest}.spec.js`]);
+    client.publish('starter', `{"id_user":"3","id_type":"2","script":"${currentTest}"}`);
     
-    var conteo=0
-    ls.stdout.on("data", data => {
-        console.log(`stdout: ${data}`);
-        if(conteo==0){
-            console.log(`publising on starter ${conteo}`);
-            client.publish('starter', `[{file:'${currentTest}'}]`);
-            conteo++;
-        } else {
-            console.log(`not publishing.  It Must be updating`);
-        }
-        
-    });
-
-    client.on('message', function (topic, message) {
+    //client.on('message', function (topic, message) {
          // message is Buffer
-        console.log(message.toString())
+      //  console.log(message.toString())
         //client.end()
-    })
-
+    //})
     
-    
-
-    ls.stdout.on("close", (data)=>{
-        console.log("Finish")
-        client.publish('terminator', `[{file:'${currentTest}'}]`);
-    })
-    
-    res.send('Test started'); //Convertir respuesta a un esquema JSON e implementar PUSH cuando la prueba termine
+    res.render('test',{test:'Cargando Test on Worker'});
+    //res.send('Test started'); //Convertir respuesta a un esquema JSON e implementar PUSH cuando la prueba termine
 });
 
 module.exports = router;
